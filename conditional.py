9246c29274a4c6a0adbca13a08e7efe8e6ee972a
codymajid <112080404+codymajid@users.noreply.github.com>
2025-03-03 13:45:01 +0500

    conditional

---
+++ conditional.py
@@ -0,0 +1,18 @@
+marks = 70;
+
+
+if(marks >= 90):
+    print("Grade A") 
+
+elif(marks >= 80):
+        print("Grade B")
+elif(marks >= 70):
+        print("Grade C")
+
+elif(marks > 70):
+    print("Grade D") 
+
+else: 
+    print(" ") 
+
+
