5aeb3c0116aae3f3090a60d4adca9e398cbbc7b4
codymajid <112080404+codymajid@users.noreply.github.com>
2025-03-15 18:20:40 +0500

    for loop

---
+++ loops/forLoop.py
@@ -0,0 +1,4 @@
+items = [1,2,3,4,6,54,3,21,324,5123,234,212,453];
+
+for elem in items : 
+    print("item", elem);
--- loops/loops.py
+++ loops/loops.py
@@ -7,9 +7,36 @@
 
 # print multiplication number n 
 
-num = int(input("Enter number 1:")); 
-seq = 1;
+# num = int(input("Enter number 1:")); 
+# seq = 1;
 
-while seq <= 10:
-    print(num, "x", seq, "=", (num * seq));
-    seq += 1; 
+# while seq <= 10:
+#     print(num, "x", seq, "=", (num * seq));
+#     seq += 1; 
+
+
+# print list using loop
+
+# nums = (1,4,7,15,44,55,312,1); 
+# idx = 0;
+
+# while idx <= len(nums) - 1: code
+#     print("nums", nums[idx]);  
+#     idx += 1; 
+
+
+# search for  number n 
+
+num = int(input("Enter Number:"));  
+nums = (1,4,7,15,44,55,312,1); 
+idx = 0;
+
+while idx <= len(nums) - 1:  
+    val = nums[idx];
+    idx += 1; 
+    if(val == num) :
+        print("yes number exists", val); 
+    elif(val != num): 
+        print('No not exists');
+    
+    
