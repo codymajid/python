4e527744b2def422d36cea4b9551e66d52ec5e8a
codymajid <112080404+codymajid@users.noreply.github.com>
2025-03-15 17:48:30 +0500

    while loop

---
+++ loops/loops.py
@@ -0,0 +1,15 @@
+# i = 1;
+
+# while i <= 100:
+#     print("x", i);  
+#     i += 1; 
+
+
+# print multiplication number n 
+
+num = int(input("Enter number 1:")); 
+seq = 1;
+
+while seq <= 10:
+    print(num, "x", seq, "=", (num * seq));
+    seq += 1; 
